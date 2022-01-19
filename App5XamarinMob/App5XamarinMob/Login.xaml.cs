94de6f07420ead3d03a74194ba67fd9bd55f2a72
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2022-01-19 12:14:32 +0300

    Добавьте файлы проекта.

---
+++ App5XamarinMob/App5XamarinMob/Login.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5XamarinMob
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void RigistrationPageBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrationPage());
+        }
+
+        private async void LoginBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
---
+++ App5XamarinMob/App5XamarinMob/RegistrationPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5XamarinMob
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void RegBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
